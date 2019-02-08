2cd8fc5d27d74b6d98ccb5102e23d9ca46e9e268
Nick Stogner <nicholas.stogner@gmail.com>
2019-02-07 21:06:49 -0500

    Fmt output with delimiters

--- main.go
+++ main.go
@@ -9,16 +9,18 @@ import (
 )
 
 func main() {
-	port := "8080"
+	port := "4000"
 	if p, ok := os.LookupEnv("PORT"); ok {
 		port = p
 	}
-	log.Printf("using PORT %v", port)
+	log.Printf("listening on PORT %v", port)
+
 	http.ListenAndServe(":"+port, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		dmp, err := httputil.DumpRequest(r, true)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println(string(dmp))
+
+		fmt.Printf("================================================================================\n%v\n", string(dmp))
 	}))
 }
